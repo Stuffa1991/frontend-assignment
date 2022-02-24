37e80a7ed249223ea543630d9c91b32eab5dd289
Staffan Enemark <sse@ordbogen.com>
2022-02-24 20:11:20 +0100

    initial commit

---
+++ example/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function UserProfile(props) {
+  return <div className="test">Hest</div>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
