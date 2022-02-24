6d2239000529c20424187f2f784e69f4e44a96ea
Staffan Enemark <sse@ordbogen.com>
2022-02-24 22:40:05 +0100

    useEffect for fetching users for component

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import users from './data.json';
 import { UserProfiles } from "./UserProfiles";
 
 function App() {
   return (
     <div className="container">
       <h1>Users</h1>
-      <UserProfiles users={users} />
+      <UserProfiles />
     </div>
   )
 }
--- example/src/UserProfiles.js
+++ example/src/UserProfiles.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import { UserProfile } from "./UserProfile";
 
 // List User profiles
-export function UserProfiles({ users }) {
+export function UserProfiles() {
+  const [users, setArray] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const usersUrl = 'https://jsonplaceholder.typicode.com/users';
+      const fetchedUsers = await fetch(usersUrl);
+      return fetchedUsers.json()
+    }
+
+    fetchUsers().then((fetchedUsers) => {
+      setArray([...fetchedUsers, []]);
+    })
+  }, []);
+
   return (
     <div className="user-profiles">
       {users.map((user, index) => (
