8052ec565d24be4fa49450f5bac10d04c4a2276a
Staffan Enemark <sse@ordbogen.com>
2022-02-24 23:06:47 +0100

    woops

--- example/src/UserProfileTable.js
+++ example/src/UserProfileTable.js
@@ -13,7 +13,7 @@ export function UserProfileTable() {
     }
 
     fetchUsers().then((fetchedUsers) => {
-      setArray([...fetchedUsers, []]);
+      setArray([...fetchedUsers]);
     })
   }, []);
 
