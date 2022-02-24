5d607ffea6629e0da97eec5795f8f71e9d117391
Staffan Enemark <sse@ordbogen.com>
2022-02-24 21:00:54 +0100

    Figure out how loops work in react, add ternarys for profile content

--- example/src/App.js
+++ example/src/App.js
@@ -1,16 +1,44 @@
 import './App.css';
+import users from './data.json';
+import React from 'react';
 
+function UserProfile({ user }) {
+  return (
+    <div className="user_profile">
+      <img className={ `user_profile__image` } src="https://placekitten.com/150/150" alt="User image"/>
+      <div className="user_profile__content">
+        <div className="user_profile__username">
+          {user.website
+            ? <a href={ `${user.website}` }>{ user.username }</a>
+            : user.username
+          }
+        </div>
+        {user.company &&
+          <div className="user_profile__company_name">
+            ({ user.company.name})
+          </div>
+        }
+      </div>
+    </div>
+  )
+}
 
-function UserProfile(props) {
-  return <img src="https://placekitten.com/150/150" alt="User image"/>
+function UserProfiles({ users }) {
+  return (
+    <div className="user_profiles">
+      {users.map((user, index) => (
+        <UserProfile key={index} user={user} />
+      ))}
+    </div>
+  )
 }
 
 function App() {
   return (
     <div className="container">
-      <UserProfile />
+      <UserProfiles users={users} />
     </div>
-  );
+  )
 }
 
 export default App;
